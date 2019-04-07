24ebecbf59f2fd1894e71f2e702c13fd5dc1cb56
irfan87 <nerve2009@yahoo.com>
2019-04-07 12:01:56 +0800

    change to the lambdas

--- case_odd_even.rb
+++ case_odd_even.rb
@@ -2,8 +2,8 @@ print "Enter the number:\n"
 num = gets.chop.to_i
 
 case num
-	when 1, 3, 5, 7, 9
-		puts "#{num} is odd"
-	when 2, 4, 6, 8, 10
+	when -> n { n % 2 == 0}
 		puts "#{num} is even"
+	else
+		puts "#{num} is odd"
 end
